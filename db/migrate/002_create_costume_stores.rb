31a44d041b644c270a66de057126d942245e7a83
Robert Salaiz <work.salaizrm@gmail.com>
2020-04-02 18:10:37 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,18 @@
+class CreateCostumeStores <ActiveRecord::Migration
+
+  def change
+    create_table :costume_stores do |r|
+      r.string :name
+      r.string :location
+      r.integer :costume_inventory
+      r.integer :number_of_employees
+      r.boolean :in_business
+      r.string :open
+      r.string :close
+    end
+  end
+
+
+end
+
 # Create your costume_stores migration here
