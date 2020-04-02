833dbd9d8307fa2872bef2b93f03622f025189f0
Robert Salaiz <work.salaizrm@gmail.com>
2020-04-02 18:41:15 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,6 +3,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change
     create_table :haunted_houses do |r|
       r.string :name
+      r.string :location
+      r.string :theme
+      r.string :price
     end
   end
 
